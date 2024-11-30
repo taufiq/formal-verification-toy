b68f41d7ed7e34ea54900c4cf566c1e9150b4541
salyaa <salya.diallo@epfl.ch>
2024-11-30 17:43:00 +0100

    fixed issue timeout

--- main.py
+++ main.py
@@ -3,6 +3,4 @@ from z3_script_generator import *
 tree = generate_vc()
 script = generate_z3_script(tree)
 export_z3pyscript("z3_script.py", script)
-run_z3pyscript("z3_script.py", timeout=10800)
-
-
+run_z3pyscript("new_z3_script.py", timeout=10800)
