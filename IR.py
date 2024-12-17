77b84353d42bc8c7c9d04212ac329e1af388ff64
salyaa <salya.diallo@epfl.ch>
2024-12-17 11:17:18 +0100

    Global terminal constants added

--- IR.py
+++ IR.py
@@ -1,8 +1,10 @@
 import copy
 from copy import deepcopy
 
+from constants import *
+
 from Basic_path import *
-from parser import *
+from parsers import *
 from Node import *
 from typing import Union
 from project_config import DEBUG, get_debug
@@ -42,11 +44,11 @@ def generate_vc(conditions, body, generated_by=None, index=None):
     for statement in body:  # Assume body all has assignments
 
         # substitute in post-cond
-        if statement.symbol == "assignment":
+        if statement.symbol == TERMINAL_ASSIGNMENT:
             variable = statement.left
             rhs_expr = statement.right
             conditions[-1] = conditions[-1].subst({variable: rhs_expr})
-        elif statement.symbol == "assumption":
+        elif statement.symbol == TERMINAL_ASSUMPTION:
             assumption = statement.left
             conditions[-1] = conditions[-1].add_implication(assumption)
     final_vc = conditions[-1].add_implication(conditions[0].left)
@@ -57,27 +59,27 @@ def generate_vc(conditions, body, generated_by=None, index=None):
 def negate_expression(expression:Node):
     if expression.eval_type == "int":
         return expression
-    if expression.eval_type == "bool":
+    if expression.eval_type == TERMINAL_BOOL:
         # TODO we can alternatively just add negation operator and let Z3 simplify it
-        if expression.symbol == "comparison":
+        if expression.symbol == TERMINAL_COMPARISON:
             if expression.op == "<":
-                return Node("comparison", ">=", expression.left, expression.right, "bool")
+                return Node(TERMINAL_COMPARISON, ">=", expression.left, expression.right, TERMINAL_BOOL)
             elif expression.op == ">":
-                return Node("comparison", "<=", expression.left, expression.right, "bool")
+                return Node(TERMINAL_COMPARISON, "<=", expression.left, expression.right, TERMINAL_BOOL)
             elif expression.op == "<=":
-                return Node("comparison", ">", expression.left, expression.right, "bool")
+                return Node(TERMINAL_COMPARISON, ">", expression.left, expression.right, TERMINAL_BOOL)
             elif expression.op == ">=":
-                return Node("comparison", "<", expression.left, expression.right, "bool")
-        elif expression.symbol == "implies":
-            return Node("boolean", "^", expression.left,
-                        negate_expression(expression.right), "bool")
-        elif expression.symbol == "boolean":
+                return Node(TERMINAL_COMPARISON, "<", expression.left, expression.right, TERMINAL_BOOL)
+        elif expression.symbol == TERMINAL_IMPLIES:
+            return Node(TERMINAL_BOOLEAN, "^", expression.left,
+                        negate_expression(expression.right), TERMINAL_BOOL)
+        elif expression.symbol == TERMINAL_BOOLEAN:
             if expression.op == "^":
-                return Node("boolean", "v", negate_expression(expression.left),
-                            negate_expression(expression.right), "bool")
+                return Node(TERMINAL_BOOLEAN, "v", negate_expression(expression.left),
+                            negate_expression(expression.right), TERMINAL_BOOL)
             elif expression.op == "v":
-                return Node("boolean", "^", negate_expression(expression.left),
-                            negate_expression(expression.right), "bool")
+                return Node(TERMINAL_BOOLEAN, "^", negate_expression(expression.left),
+                            negate_expression(expression.right), TERMINAL_BOOL)
 
 def generate_basic_paths_helper(statements, conditions, body, generated_by=None):
 
@@ -92,17 +94,17 @@ def generate_basic_paths_helper(statements, conditions, body, generated_by=None)
         statement = statements.pop(0)
         statement_type = statement.symbol
 
-        if not statement_type == "while_loop" and len(conditions) > 1:
+        if not statement_type == TERMINAL_WHILE_LOOP and len(conditions) > 1:
             raise AnnotationWithNoWhileLoop
 
-        if statement_type == "annotation":
+        if statement_type == TERMINAL_ANNOTATION:
             conditions.append(statement)
             last_statement_annotation = True
             last_annotation = statement
-        elif statement_type == "assignment" or statement_type == "assumption":
+        elif statement_type == TERMINAL_ASSIGNMENT or statement_type == TERMINAL_ASSUMPTION:
             body.append(statement)
             last_statement_annotation = False
-        elif statement_type == "if_then_else":
+        elif statement_type == TERMINAL_IF_THEN_ELSE:
             body2 = copy.deepcopy(body)
             conditions2 = copy.deepcopy(conditions)
             statements2 = copy.deepcopy(statements)
@@ -114,27 +116,27 @@ def generate_basic_paths_helper(statements, conditions, body, generated_by=None)
             if generated_by:
                 basic_paths.append(
                     Basic_path(copy.deepcopy(generated_by[2]), copy.deepcopy(body[body.index(generated_by[2]):]),
-                               copy.deepcopy(statement),
-                               copy.deepcopy(generated_by), index))
+                                copy.deepcopy(statement),
+                                copy.deepcopy(generated_by), index))
             else:
                 basic_paths.append(
                     Basic_path(copy.deepcopy(conditions[0]), copy.deepcopy(body), copy.deepcopy(statement),
-                               copy.deepcopy(generated_by), index))
+                                copy.deepcopy(generated_by), index))
 
-            body.append(Node("assumption",None,statement.left[0],None, "bool"))
+            body.append(Node(TERMINAL_ASSUMPTION,None,statement.left[0],None, TERMINAL_BOOL))
             body.append(then_statement)
 
             generated_by = ["if_statement",index,body[-1]]
 
 
-            body2.append(Node("assumption",None,negate_expression(statement.left[0]),None, "bool"))
+            body2.append(Node(TERMINAL_ASSUMPTION,None,negate_expression(statement.left[0]),None, TERMINAL_BOOL))
             body2.append(else_statement)
 
             index += 1
             generate_basic_paths_helper(statements2,conditions2,body2,["if_statement",index-1,body2[-1]])
             index += 1
             last_statement_annotation = False
-        elif statement_type == "while_loop":
+        elif statement_type == TERMINAL_WHILE_LOOP:
             if not last_statement_annotation:
                 raise WhileLoopWithNoAnnotation()
 
@@ -143,18 +145,18 @@ def generate_basic_paths_helper(statements, conditions, body, generated_by=None)
             if generated_by:
                 basic_paths.append(
                     Basic_path(copy.deepcopy(generated_by[2]), copy.deepcopy(body[body.index(generated_by[2]):]),
-                               copy.deepcopy(statement),
-                               copy.deepcopy(generated_by), index))
+                                copy.deepcopy(statement),
+                                copy.deepcopy(generated_by), index))
             else:
                 basic_paths.append(
                     Basic_path(copy.deepcopy(conditions[0]), copy.deepcopy(body), copy.deepcopy(statement),
-                               copy.deepcopy(generated_by), index))
+                                copy.deepcopy(generated_by), index))
 
             while_guard = statement.left[0]
             while_body = statement.left[1:]
 
-            while_guard_assumption = Node("assumption",None, while_guard, None, "bool")
-            not_while_guard_assumption = Node("assumption", None, negate_expression(statement.left[0]), None, "bool")
+            while_guard_assumption = Node(TERMINAL_ASSUMPTION,None, while_guard, None, TERMINAL_BOOL)
+            not_while_guard_assumption = Node(TERMINAL_ASSUMPTION, None, negate_expression(statement.left[0]), None, TERMINAL_BOOL)
 
             body2 = [while_guard_assumption]
             body = [not_while_guard_assumption]
@@ -167,8 +169,8 @@ def generate_basic_paths_helper(statements, conditions, body, generated_by=None)
             statements2 = copy.deepcopy(while_body)
 
             index += 1
-            generated_by = ["while_loop", index-1, body[0]]
-            generate_basic_paths_helper(statements2,conditions2,body2,["while_loop",index-1,body2[0]])
+            generated_by = [TERMINAL_WHILE_LOOP, index-1, body[0]]
+            generate_basic_paths_helper(statements2,conditions2,body2,[TERMINAL_WHILE_LOOP,index-1,body2[0]])
 
             index += 1
 
@@ -176,21 +178,21 @@ def generate_basic_paths_helper(statements, conditions, body, generated_by=None)
         else:
             raise Exception("Invalid expression, or expression with no effect", statement_type)
     if generated_by:
-        if generated_by[0] == "while_loop" and len(conditions) == 1 and parent_loop_invariant is not None:
+        if generated_by[0] == TERMINAL_WHILE_LOOP and len(conditions) == 1 and parent_loop_invariant is not None:
             conditions.append(copy.deepcopy(parent_loop_invariant))
-        elif generated_by[0] == "while_loop" and len(conditions) == 1:
+        elif generated_by[0] == TERMINAL_WHILE_LOOP and len(conditions) == 1:
             conditions.append(copy.deepcopy(conditions[0]))
 
         basic_paths.append(
             Basic_path(copy.deepcopy(generated_by[2]), copy.deepcopy(body[body.index(generated_by[2]):]), copy.deepcopy(conditions[-1]),
-                       copy.deepcopy(generated_by), index))
+                        copy.deepcopy(generated_by), index))
 
 
 
     else:
         basic_paths.append(
             Basic_path(copy.deepcopy(conditions[0]), copy.deepcopy(body), copy.deepcopy(conditions[-1]),
-                       copy.deepcopy(generated_by), index))
+                        copy.deepcopy(generated_by), index))
 
     verification_conditions.append(generate_vc(conditions,body,generated_by,index))
 
@@ -206,7 +208,7 @@ def generate_basic_paths():
             return
 
         # check if last statement is an annotation
-        if statements[-1].symbol != "annotation":
+        if statements[-1].symbol != TERMINAL_ANNOTATION:
             raise PostConditionError
 
         # check that nothing but variable declaration happens before the first precondition.
@@ -214,7 +216,7 @@ def generate_basic_paths():
         while statements:
             statement = statements[0]
             statement_type = statement.symbol
-            if statement_type == "annotation":
+            if statement_type == TERMINAL_ANNOTATION:
                 break
             if statement_type != "int_declaration" and statement_type != "bool_declaration" :
                 raise AnnotationOrderError()
